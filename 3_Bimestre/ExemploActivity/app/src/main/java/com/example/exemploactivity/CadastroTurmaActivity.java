060d7008db65e95d549207e059bb3eb7c21b04be
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2023-09-13 20:50:37 -0300

    Adicionado layout cadastro de turmas

---
+++ 3_Bimestre/ExemploActivity/app/src/main/java/com/example/exemploactivity/CadastroTurmaActivity.java
@@ -0,0 +1,14 @@
+package com.example.exemploactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroTurmaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_turma);
+    }
+}
