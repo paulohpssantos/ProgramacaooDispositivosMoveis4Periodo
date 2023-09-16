cafd47165311b86c3941bec5f35bf14750e397b5
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2023-09-15 22:15:35 -0300

    Adicionado implementação do AutoCompleteTextView

--- 3_Bimestre/ExemploActivity/app/src/main/java/com/example/exemploactivity/CadastroTurmaActivity.java
+++ 3_Bimestre/ExemploActivity/app/src/main/java/com/example/exemploactivity/CadastroTurmaActivity.java
@@ -7,9 +7,24 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.example.exemploactivity.modelo.Aluno;
+
+import java.util.ArrayList;
+
 public class CadastroTurmaActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView tvAddAluno;
+    private RadioButton rbAnual;
+    private RadioButton rbSemestral;
+    private RadioGroup rgSistema;
+
+    private ArrayList<Aluno>listaAlunos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,37 @@ public class CadastroTurmaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_turma);
         setTitle("Cadastro de Turma");
 
+        tvAddAluno = findViewById(R.id.tvAddAluno);
+        rbAnual = findViewById(R.id.rbAnual);
+        rbSemestral = findViewById(R.id.rbSemestral);
+        rgSistema = findViewById(R.id.rgSistema);
+
+        rgSistema.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i == rbAnual.getId()){
+                    //selecionou o Anual
+                }else{
+                    //selecionou o Semestral
+                }
+            }
+        });
+
+        carregarAlunos();
+
+    }
+
+    private void carregarAlunos(){
+        listaAlunos = Controller.getInstance().retornarAlunos();
+        String[]vetAluno = new String[listaAlunos.size()];
+        for (int i = 0; i < listaAlunos.size(); i++) {
+            Aluno aluno = listaAlunos.get(i);
+            vetAluno[i] = aluno.getNome();
+        }
+        ArrayAdapter adapter = new ArrayAdapter(CadastroTurmaActivity.this,
+                android.R.layout.simple_dropdown_item_1line, vetAluno);
+
+        tvAddAluno.setAdapter(adapter);
     }
 
     @Override
@@ -36,8 +82,17 @@ public class CadastroTurmaActivity extends AppCompatActivity {
     }
 
     private void salvarTurma() {
-        Toast.makeText(this,
-                "vc clicou no salvar!",
-                Toast.LENGTH_LONG).show();
+
+        if(rbAnual.isChecked()){
+            Toast.makeText(this,
+                    "vc selecionou o Anual",
+                    Toast.LENGTH_LONG).show();
+        }
+        if(rbSemestral.isChecked()){
+            Toast.makeText(this,
+                    "vc selecionou o Semestral",
+                    Toast.LENGTH_LONG).show();
+        }
+
     }
 }
