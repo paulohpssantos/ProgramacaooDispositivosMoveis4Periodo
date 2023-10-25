5ea41843f7365ae224329ceaef171b3f8e10e6d5
Paulo Henrique Pereira dos Santos <84387138+paulohpssantos@users.noreply.github.com>
2023-10-25 19:20:11 -0300

    Adicionado métodos de select no DAO

--- 2_Bimestre/ExemploBancoDados2/app/src/main/java/com/example/exemplobancodados/dao/AlunoDao.java
+++ 2_Bimestre/ExemploBancoDados2/app/src/main/java/com/example/exemplobancodados/dao/AlunoDao.java
@@ -2,6 +2,7 @@ package com.example.exemplobancodados.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -42,7 +43,7 @@ public class AlunoDao implements GenericDao<Aluno>{
         this.context = context;
 
         //Abrir uma conexão da BD
-        openHelper = new SQLiteDataHelper(this.context, "UNIPAR_CVEL",
+        openHelper = new SQLiteDataHelper(this.context, "UNIPAR_BD",
                 null, 1);
         //Carrega a BD e da permissão para escrever na tabela
         bd = openHelper.getWritableDatabase();
@@ -71,7 +72,7 @@ public class AlunoDao implements GenericDao<Aluno>{
 
             String[]identificador = {String.valueOf(obj.getRa())};
             return bd.update(nomeTabela, valores,
-                    "RA = ?", identificador);
+                    colunas[0]+" = ?", identificador);
 
 
         }catch (SQLException ex){
@@ -84,6 +85,10 @@ public class AlunoDao implements GenericDao<Aluno>{
     public long delete(Aluno obj) {
         try{
 
+            String[]identificador = {String.valueOf(obj.getRa())};
+            return bd.delete(nomeTabela, colunas[0]+" = ?",
+                    identificador);
+
         }catch (SQLException ex){
             Log.e("ERRO", "AlunoDao.delete(): "+ex.getMessage());
         }
@@ -92,11 +97,52 @@ public class AlunoDao implements GenericDao<Aluno>{
 
     @Override
     public ArrayList<Aluno> getAll() {
+        ArrayList<Aluno> lista = new ArrayList<>();
+        try{
+            Cursor cursor = bd.query(nomeTabela, colunas,
+                    null, null, null,
+                    null, colunas[0]);
+
+            //Verifica se é possível retornar o ponteiro para
+            // a primeira posição do cursor
+            if(cursor.moveToFirst()){
+                do{
+                    Aluno aluno = new Aluno();
+                    aluno.setRa(cursor.getInt(0));
+                    aluno.setNome(cursor.getString(1));
+
+                    lista.add(aluno);
+
+                }while (cursor.moveToNext());
+            }
+        }catch (SQLException ex){
+            Log.e("ERRO", "AlunoDao.getAll(): "+ex.getMessage());
+        }
         return null;
     }
 
     @Override
     public Aluno getById(int id) {
+        try{
+            String[]identificador = {String.valueOf(id)};
+            Cursor cursor = bd.query(nomeTabela, colunas,
+                    colunas[0]+" = ?", identificador,
+                    null, null, null);
+
+            //Verifica se é possível retornar o ponteiro para
+            // a primeira posição do cursor
+            if(cursor.moveToFirst()){
+                Aluno aluno = new Aluno();
+                aluno.setRa(cursor.getInt(0));
+                aluno.setNome(cursor.getString(1));
+
+                return aluno;
+            }
+
+        }catch (SQLException ex){
+            Log.e("ERRO", "AlunoDao.getById(): "+ex.getMessage());
+        }
+
         return null;
     }
 }
